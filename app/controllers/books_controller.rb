ab975631dffcae569400c49215440afeede8d966
718knsunnyday <718mickeeey@gmail.com>
2021-10-13 07:23:03 +0000

    バリデーション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+
   def index
     @book=Book.new
     @books=Book.all
@@ -8,32 +8,41 @@ class BooksController < ApplicationController
   def show
     @book=Book.find(params[:id])
   end
-  
+
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book=Book.new(book_params)
+    if @book.save
+      flash[:sucess] = 'Book was successfully created.'
+      redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      render :index
+    end
   end
 
   def edit
     @book=Book.find(params[:id])
   end
-  
+
   def update
     @book=Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path
+    if @book.update(book_params)
+      flash[:sucess] = 'Book was successfully updated.'
+      redirect_to book_path
+    else
+      render.update
+    end
   end
-  
+
   def destroy
     book=Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
-  private 
+
+  private
   def book_params
     params.require(:book).permit(:title,:body)
   end
-  
+
 end
